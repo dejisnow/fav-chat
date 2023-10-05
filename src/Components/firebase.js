3bb026dcd00b24839ffef83e97eeae3d84c8412c
DEJI_SNOW <deji.snow@gmail.com>
2023-10-05 08:40:22 +0100

    firebase database edit

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-import { getFirestore } from "firebase/firestore";
-
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjvK2U2RNp-wokE7jN2mLRZshWQ8qPy4I",
-  authDomain: "fav-chat-app.firebaseapp.com",
-  projectId: "fav-chat-app",
-  storageBucket: "fav-chat-app.appspot.com",
-  messagingSenderId: "130126417623",
-  appId: "1:130126417623:web:3cb7e3312c5104efd5aa92"
+  apiKey: "AIzaSyAls-wg5Z3iBM-Msz2daIyvWbdzET4AEtc",
+  authDomain: "fav-chat-7ac50.firebaseapp.com",
+  projectId: "fav-chat-7ac50",
+  storageBucket: "fav-chat-7ac50.appspot.com",
+  messagingSenderId: "785427749389",
+  appId: "1:785427749389:web:406ba6fbca670f86e35344"
 };
 
 // Initialize Firebase
